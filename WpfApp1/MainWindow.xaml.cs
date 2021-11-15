3c4831b668f07086668c479df00bd8754859bc47
Reinax <bradley.j.winson@stu.mmu.ac.uk>
2021-11-15 02:15:00 +0000

    Final Commit
    Collects Top meal data and makes all data visible on UI.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -35,9 +35,11 @@ namespace WpfApp1
             InitializeComponent();
         }
 
+        private string Topmeal;
+        private string SearchItemOne;
+        private string SearchItemTwo;
         private void ButtonExcel_Click(object sender, RoutedEventArgs e)
         {
-
             OpenFileDialog fileDialog = new OpenFileDialog();
             fileDialog.InitialDirectory = "b:\\Temp";
             fileDialog.Filter = "Excel Files(.xlsx)|*.xlsx";
@@ -48,76 +50,81 @@ namespace WpfApp1
             if (result.ToString() != string.Empty)
             {
                 txtPath.Text = fileDialog.FileName;
-                //Method for Excel create method to input excel data into datagrid.
+
                 GetData(txtPath.Text);
-
-
-
-
-                //Everything to do with Selenium Searching.
-                IWebDriver driver = new ChromeDriver();
-                driver.Navigate().GoToUrl("https://www.bbc.co.uk/food");
-
-                IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div[1]/div[1]/div/div/form/div/input[1]"));
-                driver.Manage().Window.Maximize();
-                // input food items
-                element.SendKeys("Cheese and Milk");
-                element.Submit();
-
-                //Get the search results panel that contains the link for each result.
-                driver.FindElement(By.XPath("/html/body/div[3]/div/div[1]/div[3]/div/div[4]"));
-                //Get all the links only contained within the search result panel.
-                IWebElement topMealClick = driver.FindElement(By.XPath("/html/body/div[3]/div/div[1]/div[3]/div/div[4]/div[3]/div/div[1]/a"));
-                topMealClick.Click();
-                //Submit top meal
-                IWebElement topMealName = driver.FindElement(By.XPath("/html/body/div[3]/div/div[1]/div[4]/div/div[1]/div/div[1]/div[1]/div/h1"));
-                InputCheck.Text = topMealName.Text;
-
-
-
-                // Print the text for every link in the search results.
-                /*for (int i = 0; i < searchResults.Count; i++)
-                {
-                    DataGridView.Items.Add(searchResults[i].Text);
-                    
-                }
-                */
-
-
             }
 
+        }
+
+
+        public void CollectData(string SearchItemOne, string SearchItemTwo)
+        {
+
+            //Everything to do with Selenium Searching.
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.bbc.co.uk/food");
+
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[3]/div/div[1]/div[1]/div/div/form/div/input[1]"));
+            driver.Manage().Window.Maximize();
+            // input food items
+            element.SendKeys(SearchItemOne + " And " + SearchItemTwo);
+            element.Submit();
+
+            //Get the search results panel that contains the link for each result.
+            driver.FindElement(By.XPath("/html/body/div[3]/div/div[1]/div[3]/div/div[4]"));
+            //Get all the links only contained within the search result panel.
+            IWebElement topMealClick = driver.FindElement(By.XPath("/html/body/div[3]/div/div[1]/div[3]/div/div[4]/div[3]/div/div[1]/a"));
+            topMealClick.Click();
+            //Submit top meal
+            IWebElement topMealName = driver.FindElement(By.XPath("/html/body/div[3]/div/div[1]/div[4]/div/div[1]/div/div[1]/div[1]/div/h1"));
+            Topmeal = topMealName.Text;
+            driver.Close();
         }
 
         //method
         public void GetData(string path)
         {
-            XSSFWorkbook hssfwb;
-            using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read))
+            try
             {
-                hssfwb = new XSSFWorkbook(file);
+                XSSFWorkbook hssfwb;
+                using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read))
+                {
+                    hssfwb = new XSSFWorkbook(file);
+                }
+
+
+
+                ISheet sheet = hssfwb.GetSheet("Recipes");
+                System.Collections.IEnumerator rows = sheet.GetRowEnumerator();
+                rows.MoveNext();
+                IRow HeaderRow = sheet.GetRow(0);
+
+                //get each header
+                foreach (ICell headerCell in HeaderRow)
+                {
+                    DataGridView.Columns.Add(new DataGridTextColumn { Header = headerCell.ToString(), Binding = new Binding(headerCell.ToString()) });
+                }
+
+
+                int rowCount = sheet.LastRowNum;
+                for (int i = 1; i <= rowCount; i++)
+                {
+                    IRow curRow = sheet.GetRow(i);
+
+                    var cellValue0 = curRow.GetCell(0).StringCellValue.Trim();
+                    var cellValue1 = curRow.GetCell(1).StringCellValue.Trim();
+                    var cellValue2 = curRow.GetCell(2).StringCellValue.Trim();
+                    SearchItemOne = cellValue1;
+                    SearchItemTwo = cellValue2;
+                    CollectData(SearchItemOne, SearchItemTwo);
+                    DataGridView.Items.Add(new { Name = cellValue0, IngredientOne = cellValue1, IngredientTwo = cellValue2, TopMeal = Topmeal });
+
+
+                }
             }
-
-
-            ISheet sheet = hssfwb.GetSheet("Recipes");
-            System.Collections.IEnumerator rows = sheet.GetRowEnumerator();
-            
-
-            rows.MoveNext();
-            IRow HeaderRow = sheet.GetRow(0);
-
-            //get each header
-            foreach (ICell headerCell in HeaderRow)
+            catch (Exception ex)
             {
-                DataGridView.Columns.Add(new DataGridTextColumn { Header = headerCell.ToString(), Binding = new Binding(headerCell.ToString()) });
-            }
 
-
-            //get rest of data
-            IRow FirstRow = sheet.GetRow(1);
-            foreach (ICell dataCell in FirstRow)
-            {
-                //rows
-                DataGridView.Items.Add(dataCell.ToString());
             }
 
 
